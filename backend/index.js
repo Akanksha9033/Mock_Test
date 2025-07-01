c87616552302fab2ba838e95a639031ee3eabf3d
Akanksha9033 <happychaudhary123spj@gmail.com>
2025-07-01 15:34:00 +0530

    save

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,11 @@ const app = express();
 app.use(helmet());
 app.use("/api/payment", require("./routes/paymentRoutes"));
 
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:3000", "https://www.smartlearning.co.in"],
+  credentials: true
+}));
+
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
